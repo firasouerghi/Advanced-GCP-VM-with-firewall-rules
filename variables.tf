077b21153db62c7e8ff50155d61f7c9c8195c32e
firas <firas.ouerghi@etudiant-enit.utm.tn>
2021-03-14 20:32:36 +0100

    Add required files

---
+++ variables.tf
@@ -0,0 +1,41 @@
+
+variable "project" {
+  type        = string
+  description = "project id"
+}
+
+variable "region" {
+  type        = string
+  description = "project region"
+}
+
+variable "zone" {
+  type        = string
+  description = "project zone"
+}
+
+variable "credentials_file" {
+  type        = string
+  description = "json GCP credential file"
+
+}
+variable "environment" {
+  type        = string
+  description = "environment type"
+
+}
+variable "machine_type" {
+  type = map(any)
+  default = {
+    dev  = "f1-micro"
+    test = "n1-highcpu-32"
+    prod = "n1-highcpu-32"
+  }
+  description = "type of GCP machine to use "
+}
+
+variable "ssh_key_file" {
+  type        = string
+  description = "public ssh key"
+
+}
